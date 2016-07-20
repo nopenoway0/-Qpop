edfb2a97db6cfc09d05d291eebf82b978a68dd20
nopenoway0 <kite670@yahoo.com>
2016-07-20 01:50:53 -0700

    prototype works and at a fixed  resolution with fixed cropping regions can properly detect when queue pops. Need to add more flexibility and adjustibility based on ratios of the launcher resolution

---
+++ Qpop/Image_Manipulator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+public class Image_Manipulator
+{
+    static float THRESHOLD = 60;
+    public static bool Compare_Image(Bitmap imgA, Bitmap imgB)
+    {
+        return Compare_Image(imgA, imgB, 100);
+    }
+
+    static void SET_THRESHOLD(float a)
+    {
+        THRESHOLD = a;
+    }
+
+    public static bool Compare_Image(Bitmap imgA, Bitmap imgB, int rescale_size)
+    {
+        int total_pixels = rescale_size * rescale_size;
+        Bitmap reducedA = new Bitmap(imgA, rescale_size, rescale_size);
+        //reducedA.Save("reducedA.bmp"); // del
+        Bitmap reducedB = new Bitmap(imgB, rescale_size, rescale_size);
+        //reducedB.Save("reducedB.bmp"); // del
+        float diff = 0;
+        // Courtesy of https://rosettacode.org/wiki/Percentage_difference_between_images#C.23
+        for (int y = 0; y < reducedA.Height; y++)
+        {
+            for (int x = 0; x < reducedA.Width; x++)
+            {
+                diff += (float)Math.Abs(reducedA.GetPixel(x, y).R - reducedB.GetPixel(x, y).R) / 255;
+                diff += (float)Math.Abs(reducedA.GetPixel(x, y).G - reducedB.GetPixel(x, y).G) / 255;
+                diff += (float)Math.Abs(reducedA.GetPixel(x, y).B - reducedB.GetPixel(x, y).B) / 255;
+            }
+        }
+        float perc_matched = ((total_pixels - diff) / total_pixels) * 100;
+        Console.Write(string.Concat("Pixel Match: ", total_pixels - diff, "/", total_pixels," Pixels\n", perc_matched, "%\n"));
+        if (perc_matched >= THRESHOLD) return true;
+        else return false;
+    }
+
+    public static bool Compare_Image_Section(Bitmap imgA, Bitmap imgB, Rectangle section_imgA, Rectangle section_imgB)
+    {
+        Bitmap croppedA = (Bitmap)imgA.Clone(section_imgA, imgA.PixelFormat);
+        croppedA.Save("CroppedA.bmp"); // del
+        Bitmap croppedB = (Bitmap)imgB.Clone(section_imgB, imgB.PixelFormat);
+        croppedB.Save("CroppedB.bmp"); // del
+        return Compare_Image(croppedA, croppedB);
+    }
+}
--- Qpop/Program.cs
+++ Qpop/Program.cs
@@ -34,10 +34,22 @@ namespace Qpop
             //SnapshotHelper.Print_Process_Window_Dimensions(cn_process);
             //End Test
 
-            //Test Launcher Copy
-            Bitmap screen = SnapshotHelper.Take_Snapshot_Process(cn_process, 491, 436, 183, 41);
+            //Test Launcher Copy and Compare
+            //Bitmap screen = SnapshotHelper.Take_Snapshot_Process(cn_process, 491, 436, 183, 41);
+            //Bitmap refScreen = new Bitmap("reference.PNG");
+            //Bitmap queue_img = new Bitmap("join.bmp");
+            //Image_Manipulator.Compare_Image_Section(refScreen, queue_img, new Rectangle(491, 436, 183, 41), new Rectangle(0, 0, queue_img.Width, queue_img.Height));
             //End Test
-            
+
+            //Loop for test comparison of live results
+            while (true)
+            {
+                Bitmap screen = SnapshotHelper.Take_Snapshot_Process(cn_process, 491, 436, 183, 41);
+                Bitmap queue_img = new Bitmap("join.bmp");
+                Console.Write(Image_Manipulator.Compare_Image(screen, queue_img));
+                System.Threading.Thread.Sleep(2000);
+            }
+            //End Test
 
             System.Console.ReadLine();
         }
