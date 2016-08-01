4421ba74fd89b4804b18f31728d982f6bc8abe24
nopenoway0 <kite670@yahoo.com>
2016-08-01 15:19:43 -0700

    began adding multiple program support

--- Qpop/Program_Importer.cs
+++ Qpop/Program_Importer.cs
@@ -4,43 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections;
 
 namespace Qpop
 {
     class Program_Importer
     {
-        string[] program_list;
-        string file;
+        public static List<Program_Profile> programs = new List<Program_Profile>();
         public static void Import_Programs()
         {
-            //TODO: Add support for comments in text files
             Console.WriteLine("Importing programs");
             FileStream read = File.OpenRead("data.txt");
             string contentsS;
+            int[] dimensions;
             byte[] contents = new byte[read.Length];
             byte temp;
             bool detected_token = false;
-            int x = 0;
+            int x = 0; int EOF;
+            string[] sections;
             while (x < read.Length)
             {
-                temp = (byte)read.ReadByte();
-                if(temp != '"' && detected_token == false) {
+                EOF = read.ReadByte();
+                temp = (byte)EOF;
+                if (EOF == -1) break;
+                else if (temp != '"' && detected_token == false)
+                {
                     contents[x] = temp;
                     x++;
                 }
-                else if(temp == '"' && detected_token == true)
+                else if (temp == '"' && detected_token == true)
                 {
                     detected_token = false;
                 }
-                else if(temp == '"' && detected_token == false)
+                else if (temp == '"' && detected_token == false)
                 {
                     detected_token = true;
                 }
             }
             read.Close();
             read.Dispose();
+
+            //TODO: Allow for more than 1 profile, change style of loading
             contentsS = ASCIIEncoding.ASCII.GetString(contents);
-            Console.WriteLine(contentsS);
+            contentsS = contentsS.Replace(" ", "");
+            sections = contentsS.Split('\n');
+            dimensions = Image_Manipulator.Get_Image_Dimensions(sections[3]);
+            Program_Profile profile = new Program_Profile(Int32.Parse(sections[1]), Int32.Parse(sections[2]), dimensions[0], dimensions[1], sections[3]);
+            profile.Set_Name(sections[0].Replace("\r", "")); //\r causing problems?
+            if (profile != null) programs.Add(profile);
         }
     }
 }
