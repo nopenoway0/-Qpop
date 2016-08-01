5cb69261794ed58c8b68f47b72ed8f83843cadb9
nopenoway0 <kite670@yahoo.com>
2016-08-01 11:42:36 -0700

    Added support for comments in text file

--- Qpop/Program_Importer.cs
+++ Qpop/Program_Importer.cs
@@ -13,7 +13,34 @@ namespace Qpop
         string file;
         public static void Import_Programs()
         {
+            //TODO: Add support for comments in text files
+            Console.WriteLine("Importing programs");
             FileStream read = File.OpenRead("data.txt");
+            string contentsS;
+            byte[] contents = new byte[read.Length];
+            byte temp;
+            bool detected_token = false;
+            int x = 0;
+            while (x < read.Length)
+            {
+                temp = (byte)read.ReadByte();
+                if(temp != '"' && detected_token == false) {
+                    contents[x] = temp;
+                    x++;
+                }
+                else if(temp == '"' && detected_token == true)
+                {
+                    detected_token = false;
+                }
+                else if(temp == '"' && detected_token == false)
+                {
+                    detected_token = true;
+                }
+            }
+            read.Close();
+            read.Dispose();
+            contentsS = ASCIIEncoding.ASCII.GetString(contents);
+            Console.WriteLine(contentsS);
         }
     }
 }
