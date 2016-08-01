c3b8a7f9b54c3599908b428e10eb6ec7dcbb757c
nopenoway0 <kite670@yahoo.com>
2016-08-01 11:13:58 -0700

    building base for importing programs

---
+++ Qpop/Program_Importer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Qpop
+{
+    class Program_Importer
+    {
+        string[] program_list;
+        string file;
+        public static void Import_Programs()
+        {
+            FileStream read = File.OpenRead("data.txt");
+        }
+    }
+}
