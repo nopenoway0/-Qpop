532f5ba8122514019281201735316483eba406a6
nopenoway0 <kite670@yahoo.com>
2016-08-01 20:33:26 -0700

    Fixed the buttons to auto resize

--- Qpop/Program_Selection.cs
+++ Qpop/Program_Selection.cs
@@ -14,8 +14,9 @@ namespace Qpop
     public partial class Program_Selection : Form
     {
         public delegate void Button_Add_Enabler(string button_name);
-        public int num_buttons = 2;
+        public int button_count = 0;
         private TextBox tstBox = new TextBox();
+        public static Thread main_runner;
         private TableLayoutPanel tpane = new TableLayoutPanel();
         [STAThread]
         public static void Main()
@@ -27,7 +28,7 @@ namespace Qpop
         public Program_Selection()
         {
             InitializeComponent();
-            Thread main_runner = new Thread(delegate ()
+            main_runner = new Thread(delegate ()
             {
                 Program.Start(this);
             });
@@ -53,10 +54,25 @@ namespace Qpop
                 this.Invoke(new Button_Add_Enabler(Add_Button), new object[] { button_name });
                 return;
             }
-            else{ Button b1 = new Button();
-                b1.Text = button_name;
-                b1.AutoSize = true;
-                tpane.Controls.Add(b1); }
+            Button b1 = new Button();
+            b1.Click += new EventHandler(Get_Number);
+            b1.Text = button_name;
+            b1.Name = string.Concat("game_button_", button_count);
+            b1.AutoSize = true;
+            tpane.Controls.Add(b1);
+            button_count++;
         }
+
+        public void Get_Number(object sender, EventArgs e)
+        {
+            Button b1 = (Button)sender;
+            Program.choice = Int32.Parse(b1.Name.Last().ToString());
+        }
+
+        public static Thread Get_Runner()
+        {
+            return main_runner;
+        }
+        }
+
     }
-}
