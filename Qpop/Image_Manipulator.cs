4113601598d4715681cdb448cc217a7490f78534
nopenoway0 <kite670@yahoo.com>
2016-08-11 02:30:47 -0700

    Added method to extract the pixel values of an image for later comparison. Allows a sort of caching, for the reference image, instead of making comparisons by extracting the information from the pixels of both images, the references data is already extracted. Dropped CPU usage from 29% to 13% on average

--- Qpop/Image_Manipulator.cs
+++ Qpop/Image_Manipulator.cs
@@ -7,15 +7,11 @@ using System.Drawing;
 
 public class Image_Manipulator
 {
-    static float THRESHOLD = 65;
+    public static float THRESHOLD = 65;
+    public static int DEFAULT_RESIZE = 150;
     public static bool Compare_Image(Bitmap imgA, Bitmap imgB)
     {
-        return Compare_Image(imgA, imgB, 200);
-    }
-
-    static void SET_THRESHOLD(float a)
-    {
-        THRESHOLD = a;
+        return Compare_Image(imgA, imgB, DEFAULT_RESIZE);
     }
 
     public static bool Compare_Image(Bitmap imgA, Bitmap imgB, int rescale_size)
@@ -42,6 +38,45 @@ public class Image_Manipulator
         else return false;
     }
 
+    public static bool Compare_Image(byte[,,] pixelValues, Bitmap imgB, int rescale_size)
+    {
+        int total_pixels = rescale_size * rescale_size;
+        Bitmap reducedB = new Bitmap(imgB, rescale_size, rescale_size);
+        //reducedB.Save("reducedB.bmp"); // del
+        float diff = 0;
+        // Courtesy of https://rosettacode.org/wiki/Percentage_difference_between_images#C.23
+        for (int y = 0; y < reducedB.Height; y++)
+        {
+            for (int x = 0; x < reducedB.Width; x++)
+            {
+                diff += (float)Math.Abs(pixelValues[x, y, 0] - reducedB.GetPixel(x, y).R) / 255;
+                diff += (float)Math.Abs(pixelValues[x, y, 1] - reducedB.GetPixel(x, y).G) / 255;
+                diff += (float)Math.Abs(pixelValues[x, y, 2] - reducedB.GetPixel(x, y).B) / 255;
+            }
+        }
+        float perc_matched = ((total_pixels - diff) / total_pixels) * 100;
+        Console.WriteLine("\n" + perc_matched);
+        //Console.Write(string.Concat("Pixel Match: ", total_pixels - diff, "/", total_pixels," Pixels\n", perc_matched, "%\n"));
+        if (perc_matched >= THRESHOLD) return true;
+        else return false;
+    }
+
+    public static byte[,,] Store_Image_Value(Bitmap imgA, int rescale_size)
+    {
+        Bitmap reducedA = new Bitmap(imgA, rescale_size, rescale_size);
+        byte[,,] pixelValues = new byte[reducedA.Height, reducedA.Width, 3];
+        for (int y = 0; y < reducedA.Height; y++)
+         {
+            for (int x = 0; x < reducedA.Width; x++)
+         {
+            pixelValues[x, y, 0] = reducedA.GetPixel(x, y).R;
+            pixelValues[x, y, 1] = reducedA.GetPixel(x, y).G;
+            pixelValues[x, y, 2] = reducedA.GetPixel(x, y).B;
+        }
+        }
+        return pixelValues;
+    }
+
     public static bool Compare_Image_Section(Bitmap imgA, Bitmap imgB, Rectangle section_imgA, Rectangle section_imgB)
     {
         Bitmap croppedA = (Bitmap)imgA.Clone(section_imgA, imgA.PixelFormat);
@@ -49,6 +84,12 @@ public class Image_Manipulator
         Bitmap croppedB = (Bitmap)imgB.Clone(section_imgB, imgB.PixelFormat);
         //croppedB.Save("CroppedB.bmp"); // del
         return Compare_Image(croppedA, croppedB);
+    }
+
+    public static bool Compare_Image_Section(Bitmap imgA, Rectangle section_imgA, byte[,,] pixelValues)
+    {
+        Bitmap croppedA = (Bitmap)imgA.Clone(section_imgA, imgA.PixelFormat);
+        return Compare_Image(pixelValues, croppedA, Image_Manipulator.DEFAULT_RESIZE);
     }
 
     // Returns the dimensions of the filename, no error handling, x,y format, or width then height
